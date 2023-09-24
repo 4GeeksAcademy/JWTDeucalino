aff9b18398ddf21ec034d808c6ea0e08780161eb
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-09-24 21:31:49 +0000

    Private view, log in and signup views set, modals ready, need to define endpoints and token

---
+++ src/front/js/pages/private.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Private = () => {
+	return (
+        <div class="container-fluid">
+          <div class="card">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1k_KkOMz8I3h2ImdtiZW1qhQ8f5QWuxkjA&usqp=CAU" class="card-img-top" alt="..."/>
+             <div class="card-body">
+             <h5 class="card-title">4M Likes</h5>
+             <p class="card-text">This is how bored apes yacht club make flufy cats look less interesting.</p>
+          </div>
+        </div>
+        </div>      
+	);
+};
